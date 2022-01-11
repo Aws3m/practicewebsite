d0e3c6fcc715334a528520cce654fe52553e5e34
Aws3m <semradijs@hotmail.com>
2022-01-11 05:59:31 +0100

    changes 11/1/22: -finished temporarily about page -tweaks to footer

--- scripts/getCurrentYearForCopyright.ts
+++
@@ -1,6 +0,0 @@
-
-let copyrightSign = document.createElement("p");
-let date = new Date();
-let year = date.getFullYear();
-const copyrightDiv = copyrightSign.textContent = "Copyright &copy; " + year;
-document.getElementById("copyright").append("copyrightDiv");
