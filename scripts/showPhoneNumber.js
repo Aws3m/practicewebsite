82dbb7b91dd569976877bd2c207117c8e6cb9910
Aws3m <semradijs@hotmail.com>
2022-02-04 15:10:53 +0100

    added style to contactpage call me/textme and removed js file

--- scripts/showPhoneNumber.js
+++
@@ -1,6 +0,0 @@
-const telefoonel = document.createElement("p");
-telefoonel.classList.add("telefoon");
-telefoonel.classList.add("clay-style");
-telefoonel.innerText = "0000000000";
-const icons = document.querySelector(".icons");
-icons.parentNode.insertBefore(telefoonel, icons);
