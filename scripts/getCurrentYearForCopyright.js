ff8767dd82ef353bb88cbb31dd3a9c29cb67e28a
SemRadoes <semradijs@hotmail.com>
2024-06-30 23:50:14 +0200

    progress

--- scripts/getCurrentYearForCopyright.js
+++ scripts/getCurrentYearForCopyright.js
@@ -1,4 +1,4 @@
 const copyrightSign = document.createElement("p");
 const year = new Date().getFullYear();
 copyrightSign.innerHTML = "Copyright &copy; " + year; // InnerHTML wordt gebruikt omdat het anders &copy; letterlijk uitprint
-document.getElementById("copyright").appendChild(copyrightSign);
+$("#copyright").appendChild(copyrightSign);
