70e2ea3c2168628b1c76a39586d2c7539b8e8858
SemRadoes <semradijs@hotmail.com>
2024-07-29 15:41:19 +0200

    last bug on landing page: mobile cross after opening menu doesnt appear

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,4 +1,11 @@
 
-const toggleMenu = () => {
+const toggleMenu = (status) => {
     $('.slidetoggle').slideToggle('fast');
+    if(status === "open"){
+        $('#openmenu').css('display', 'none');
+        $('#closedmenu').css('display', 'block');
+    } else {
+        $('#closedmenu').css('display', 'none');
+        $('#openmenu').css('display', 'flex');
+    }
 }
