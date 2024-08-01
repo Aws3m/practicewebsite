0bbc2d1954c3a1f192576bc02d508ef58a2ed3a5
SemRadoes <semradijs@hotmail.com>
2024-08-01 22:15:35 +0200

    latest changes

--- scripts/getCurrentYearForCopyright.js
+++ scripts/getCurrentYearForCopyright.js
@@ -1,4 +1,4 @@
 const copyrightSign = document.createElement("p");
 const year = new Date().getFullYear();
-copyrightSign.innerHTML = " Designed by Awsem Webdev &copy; " + year + "<br> <span style='font-size: .6em; text-decoration: underline;'>privacy policy</span><br>"; // InnerHTML wordt gebruikt omdat het anders &copy; letterlijk uitprint
+copyrightSign.innerHTML = "&copy; " + year + "<br> <span style='font-size: .6em; text-decoration: underline;'>privacy policy</span><br>"; // InnerHTML wordt gebruikt omdat het anders &copy; letterlijk uitprint
 $('#copyright').append(copyrightSign);
