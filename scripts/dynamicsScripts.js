b63dddbb9da99a9a2ebeffebe07539c6ba3f9173
SemRadoes <semradijs@hotmail.com>
2024-08-01 16:28:48 +0200

    latest changes

--- scripts/dynamicsScripts.js
+++ scripts/dynamicsScripts.js
@@ -1,9 +1,3 @@
-// $('#worktogether').on('click',() => {
-//     $('#worktogether').css(
-//         "transform", 'scale(0.95)'
-//       );
-// });
-
 
 setInterval($('#handshake').addClass('animate__animated animate__swing'), 3000);
 setInterval($('#handshake').removeClass('animate__animated animate__swing'), 4000);
