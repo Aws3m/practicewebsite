3b766d19abe1697a5058c154ed5073afca360600
SemRadoes <semradijs@hotmail.com>
2024-08-16 16:46:39 +0200

    trying to fix a footer bug

--- scripts/dynamicsScripts.js
+++ scripts/dynamicsScripts.js
@@ -1,4 +1,17 @@
-//send the form
+//change sendbutton colors
+changeColors = (sendbutton, sendsvg) => {
+    $(`.${sendbutton}`).css('color', 'rgb(7, 151, 232)');
+    $(`.${sendbutton}`).css('border-color', 'rgb(7, 151, 232)');
+    $(`.${sendsvg}`).css('color', 'rgb(7, 151, 232)');
+    setTimeout(() => {
+        $(`.${sendbutton}`).css('color', 'rgb(64,64,102)');
+    $(`.${sendbutton}`).css('border-color', 'rgb(64,64,102)');
+    $(`.${sendsvg}`).css('color', 'rgb(64,64,102)');
+    }, 1000);
+};
+
+//errors
+
 
 
 
