dd5d3b9ae983fcfa8051a5d9de7ff4bceba5f514
SemRadoes <semradijs@hotmail.com>
2024-08-17 18:46:09 +0200

    finishign touches to contatc page

--- scripts/dynamicsScripts.js
+++ scripts/dynamicsScripts.js
@@ -1,14 +1,14 @@
 //change sendbutton colors
-changeColors = (sendbutton, sendsvg) => {
-    $(`.${sendbutton}`).css('color', 'rgb(7, 151, 232)');
-    $(`.${sendbutton}`).css('border-color', 'rgb(7, 151, 232)');
-    $(`.${sendsvg}`).css('color', 'rgb(7, 151, 232)');
-    setTimeout(() => {
-        $(`.${sendbutton}`).css('color', 'rgb(64,64,102)');
-    $(`.${sendbutton}`).css('border-color', 'rgb(64,64,102)');
-    $(`.${sendsvg}`).css('color', 'rgb(64,64,102)');
-    }, 1000);
-};
+// changeColors = (sendbutton, sendsvg) => {
+//     $(`.${sendbutton}`).css('color', 'rgb(7, 151, 232)');
+//     $(`.${sendbutton}`).css('border-color', 'rgb(7, 151, 232)');
+//     $(`.${sendsvg}`).css('color', 'rgb(7, 151, 232)');
+//     setTimeout(() => {
+//         $(`.${sendbutton}`).css('color', 'rgb(64,64,102)');
+//     $(`.${sendbutton}`).css('border-color', 'rgb(64,64,102)');
+//     $(`.${sendsvg}`).css('color', 'rgb(64,64,102)');
+//     }, 1000);
+// };
 
 //errors
 
